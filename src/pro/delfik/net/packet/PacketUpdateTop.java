00363073acd2b3446e4cf2b94a1bb5244d236802
6oogle <32604831+6oogle@users.noreply.github.com>
2018-07-16 23:48:12 +0300

    Добавил смерти

--- src/pro/delfik/net/packet/PacketUpdateTop.java
+++ src/pro/delfik/net/packet/PacketUpdateTop.java
@@ -9,7 +9,7 @@ public class PacketUpdateTop extends Packet{
 
 	private final boolean win;
 
-	private final int beds;
+	private final int beds, deaths;
 
 	public PacketUpdateTop(String serialize){
 		super("updatetop");
@@ -17,13 +17,15 @@ public class PacketUpdateTop extends Packet{
 		this.nick = split[0];
 		this.win = Converter.toBoolean(split[1]);
 		this.beds = Converter.toInt(split[2]);
+		this.deaths = Converter.toInt(split[3]);
 	}
 
-	public PacketUpdateTop(String nick, boolean win, int beds) {
+	public PacketUpdateTop(String nick, boolean win, int beds, int deaths) {
 		super("updatetop");
 		this.nick = nick;
 		this.win = win;
 		this.beds = beds;
+		this.deaths = deaths;
 	}
 
 	public String getNick() {
@@ -40,6 +42,6 @@ public class PacketUpdateTop extends Packet{
 
 	@Override
 	protected String encode() {
-		return nick + "?" + win + "?" + beds;
+		return nick + "?" + win + "?" + beds + "?" + deaths;
 	}
 }
