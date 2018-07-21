da346300fc0785cd6e96343b81e5e4d398b1dd7f
6oogle <32604831+6oogle@users.noreply.github.com>
2018-07-22 01:51:37 +0300

    123

--- src/pro/delfik/util/FileConverter.java
+++ src/pro/delfik/util/FileConverter.java
@@ -2,9 +2,11 @@ package pro.delfik.util;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Base64;
 
@@ -12,9 +14,8 @@ public class FileConverter {
 	public static void write(File file, String str){
 		try{
 			str = new String(Base64.getDecoder().decode(str));
-			BufferedOutputStream writer = new BufferedOutputStream(new FileOutputStream(file));
-			for(char c : str.toCharArray())
-				writer.write(c);
+			BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+			writer.write(str);
 			writer.flush();
 			writer.close();
 		}catch (IOException ex){
