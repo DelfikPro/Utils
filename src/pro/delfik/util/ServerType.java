dcdaefacabd96e3c785b09fccdb013ce41b9b3e9
6oogle <32604831+6oogle@users.noreply.github.com>
2018-07-24 11:10:44 +0300

    Add TEST & B

--- src/pro/delfik/util/ServerType.java
+++ src/pro/delfik/util/ServerType.java
@@ -3,9 +3,11 @@ package pro.delfik.util;
 public enum ServerType {
 	UNKNOWN,
 	LOBBY,
+	TEST,
 	PVP,
 	BW,
-	SF;
+	SF,
+	B;
 
 	public static ServerType getType(String line){
 		for(ServerType type : values())
