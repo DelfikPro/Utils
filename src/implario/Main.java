e79dc21f7b891a2f9ad39c401cb1a2bcc6fd1036
6oogle <32604831+6oogle@users.noreply.github.com>
2018-09-03 22:21:08 +0300

    123

--- src/implario/Main.java
+++ src/implario/Main.java
@@ -1,12 +1,7 @@
 package implario;
 
 import implario.net.Packet;
-import implario.net.packet.PacketInit;
 import implario.net.packet.PacketTop;
-import implario.net.packet.PacketUser;
-import implario.util.ByteUnzip;
-import implario.util.Coder;
-import implario.util.Rank;
 
 public class Main{
 
