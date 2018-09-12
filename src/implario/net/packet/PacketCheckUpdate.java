5bc072c6165dea0673d63b20004b336ad1787461
DelfikPro <33707322+DelfikPro@users.noreply.github.com>
2018-09-12 16:43:18 +0300

    Инициализирующий коммит

--- src/implario/net/packet/PacketCheckUpdate.java
+++ src/implario/net/packet/PacketCheckUpdate.java
@@ -3,4 +3,21 @@ package implario.net.packet;
 import implario.net.Packet;
 
 public class PacketCheckUpdate extends Packet {
+
+	private String plugin;
+	private long time;
+
+	public PacketCheckUpdate() {}
+
+	public PacketCheckUpdate(String plugin, long time) {
+		this.plugin = plugin;
+		this.time = time;
+	}
+
+	public long getTime() {
+		return time;
+	}
+	public String getPlugin() {
+		return plugin;
+	}
 }
